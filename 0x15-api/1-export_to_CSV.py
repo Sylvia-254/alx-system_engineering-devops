210b4e08ddb19bd7369124283988ef2529d184ca
Sylvia-254 <cheptoosylvia@gmail.com>
2024-08-12 09:08:05 +0000

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
-0-gather_data_from_an_API.py: A script to fetch and display an employee's
-TODO list progress.
+1-export_to_CSV.py: A script to fetch and export an employee's TODO list
+progress in CSV format.
 """
 
+import csv
 import requests
 import sys
 
@@ -21,7 +22,7 @@ def get_employee_name(employee_id):
     url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
     response = requests.get(url)
     if response.status_code == 200:
-        return response.json().get("name")
+        return response.json().get("username")
     return None
 
 
@@ -42,32 +43,44 @@ def get_todo_list(employee_id):
     return []
 
 
+def export_to_csv(employee_id, username, todo_list):
+    """
+    Export the TODO list to a CSV file.
+
+    Args:
+        employee_id (int): The employee's ID.
+        username (str): The username of the employee.
+        todo_list (list): The list of TODO items.
+    """
+    filename = f"{employee_id}.csv"
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for task in todo_list:
+            writer.writerow([employee_id, username,
+                             task.get("completed"), task.get("title")])
+
+
 def main(employee_id):
     """
-    Main function to fetch and display the TODO list progress of an employee.
+    Main function to fetch and export the TODO list progress of an employee.
 
     Args:
         employee_id (int): The employee's ID.
     """
-    employee_name = get_employee_name(employee_id)
-    if employee_name is None:
+    username = get_employee_name(employee_id)
+    if username is None:
         print(f"Employee with ID {employee_id} not found.")
         return
 
     todo_list = get_todo_list(employee_id)
-    total_tasks = len(todo_list)
-    done_tasks = [task for task in todo_list if task.get("completed")]
-    done_tasks_count = len(done_tasks)
-
-    print(f"Employee {employee_name} is done with tasks({done_tasks_count}/"
-          f"{total_tasks}):")
-    for task in done_tasks:
-        print(f"\t {task.get('title')}")
+    export_to_csv(employee_id, username, todo_list)
+    print(f"Data for employee ID {employee_id} has been exported to "
+          f"{employee_id}.csv")
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
+        print("Usage: ./1-export_to_CSV.py <employee_id>")
         sys.exit(1)
 
     try:
